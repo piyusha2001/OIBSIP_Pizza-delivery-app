5fd6f108187b4c9233b976c376e64d7475a18843
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-17 12:57:46 +0530

    mongodb connection called

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connection = require('./db');
+
+//connect database
+connection();
 
 //middleware
 app.use(express.json());
