d7f2644bf8ede95232f48b8ce8c00fa37eee454e
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-17 15:36:36 +0530

    auth and login route imported

--- server/index.js
+++ server/index.js
@@ -3,13 +3,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require('./db');
-
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
 //connect database
 connection();
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
