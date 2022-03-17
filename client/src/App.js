10ec29f0f94db9a46a9e77c3c9fc93b6596cbd6c
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-17 19:24:40 +0530

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Main from './components/Main';
+import Signup from './components/Signup';
+
 function App() {
+	const user = localStorage.getItem('token');
+
 	return (
-		<div className='App'>
-			<h1>Hello world</h1>
-		</div>
+		<Routes>
+			{user && <Route path='/' exact element={<Main />} />}
+			<Route path='/signup' exact element={<Signup />} />
+			<Route path='/login' exact element={<Login />} />
+			<Route path='/' element={<Navigate replace to='/login' />} />
+		</Routes>
 	);
 }
 
