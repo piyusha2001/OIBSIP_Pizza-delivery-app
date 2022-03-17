7364177fb2bea6db580def9b0639283e0945bc2d
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-17 19:25:58 +0530

    error resolved

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { User, validate } = require('../models/user');
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
 	try {
 		const { error } = validate(req.body);
-		if (error) {
+		if (error)
 			return res.status(400).send({ message: error.details[0].message });
-		}
+
 		const user = await User.findOne({ email: req.body.email });
 		if (user)
 			return res
