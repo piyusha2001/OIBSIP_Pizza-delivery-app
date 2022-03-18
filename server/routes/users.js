e1128526a48ba602e92e8ea6ca323e4ec14cf45f
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-18 18:03:10 +0530

    created token and url for verification

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { User, validate } = require('../models/user');
 const bcrypt = require('bcrypt');
+const Token = require('../models/token');
+const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 router.post('/', async (req, res) => {
 	try {
@@ -8,7 +11,7 @@ router.post('/', async (req, res) => {
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User.findOne({ email: req.body.email });
+		let user = await User.findOne({ email: req.body.email });
 		if (user)
 			return res
 				.status(409)
@@ -17,8 +20,19 @@ router.post('/', async (req, res) => {
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-		await new User({ ...req.body, password: hashPassword }).save();
-		res.status(201).send({ message: 'User created successfully' });
+		user = await new User({ ...req.body, password: hashPassword }).save();
+
+		const token = await new Token({
+			userId: user._id,
+			token: crypto.randomBytes(32).toString('hex'),
+		}).save();
+
+		const url = `${process.env.BASE_URL}users/${user.id}/verify/${token.token}`;
+		await sendEmail(user.email, 'Verify Email', url);
+
+		res.status(201).send({
+			message: 'An Email sent to your account please verify',
+		});
 	} catch (error) {
 		res.status(500).send({ message: 'Internal Server Error' });
 	}
