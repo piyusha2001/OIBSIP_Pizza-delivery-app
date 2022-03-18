0c0e1fd1448b04bfdb0dee3b0ff6c36f640de435
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 01:45:01 +0530

    Login button added and linked

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
-
 const Main = () => {
+	let navigate = useNavigate();
+	const user = localStorage.getItem('token');
 	const handleLogout = () => {
 		localStorage.removeItem('token');
 		window.location.reload();
@@ -9,10 +11,26 @@ const Main = () => {
 	return (
 		<div className={styles.main_container}>
 			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
-				<button className={styles.white_btn} onClick={handleLogout}>
-					Logout
-				</button>
+				<h1>Pizza App</h1>
+				<div>
+					{user ? (
+						<button
+							className={styles.white_btn}
+							onClick={handleLogout}
+						>
+							Logout
+						</button>
+					) : (
+						<button
+							className={styles.white_btn}
+							onClick={() => {
+								navigate('/login');
+							}}
+						>
+							Login
+						</button>
+					)}
+				</div>
 			</nav>
 		</div>
 	);
