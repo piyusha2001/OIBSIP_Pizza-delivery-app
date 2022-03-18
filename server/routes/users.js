63ca7715738da3e566f528ec995dce16583eb5cf
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-18 18:09:57 +0530

    update user schema after user is verified

--- server/routes/users.js
+++ server/routes/users.js
@@ -38,4 +38,24 @@ router.post('/', async (req, res) => {
 	}
 });
 
+router.get('/:id/verify/:token/', async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id });
+		if (!user) return res.status(400).send({ message: 'Invalid link' });
+
+		const token = await Token.findOne({
+			userId: user._id,
+			token: req.params.token,
+		});
+		if (!token) return res.status(400).send({ message: 'Invalid link' });
+
+		await User.updateOne({ _id: user._id, verified: true });
+		await token.remove();
+
+		res.status(200).send({ message: 'Email verified successfully' });
+	} catch (error) {
+		res.status(500).send({ message: 'Internal Server Error' });
+	}
+});
+
 module.exports = router;
