d5c215a80d6a2628ff85e4862a192a5ae0707e8e
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 02:07:49 +0530

    url verified by checking id and token

--- server/routes/passwordReset.js
+++ server/routes/passwordReset.js
@@ -41,4 +41,22 @@ router.post('/', async (req, res) => {
 	}
 });
 
+//verify url is valid
+router.get('/:id/:token', async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id });
+		if (!user) return res.status(400).send({ message: 'Invalid link' });
+
+		const token = await Token.findOne({
+			userId: user._id,
+			token: req.params.token,
+		});
+		if (!token) return res.status(400).send({ message: 'Invalid link' });
+
+		res.status(200).send('Valid Url');
+	} catch (error) {
+		res.status(500).send({ message: 'Internal Server Error' });
+	}
+});
+
 module.exports = router;
