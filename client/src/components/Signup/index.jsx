255409ce1259afa885e424b0d4b0bfe81c1f6ce4
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-18 18:43:17 +0530

    useNavigate removed

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
 const Signup = () => {
