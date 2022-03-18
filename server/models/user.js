7ac696c3c1515bd7f43c46abb7f3e553b5912dd1
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-18 16:51:38 +0530

    added verified paramter to user schema

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
 	lastName: { type: 'string', required: true },
 	email: { type: 'string', required: true },
 	password: { type: 'string', required: true },
+	verified: { type: 'boolean', default: false },
 });
 
 userSchema.methods.generateAuthToken = function () {
