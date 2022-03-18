7b9d31ed8fa73705ad39431c2dc203b796655ff9
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 02:49:12 +0530

    password reset route added

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import EmailVerify from './components/EmailVerify';
 import ForgotPassword from './components/ForgotPassword';
 import Login from './components/Login';
 import Main from './components/Main';
+import PasswordReset from './components/PasswordReset';
 import Signup from './components/Signup';
-
 function App() {
 	// const user = localStorage.getItem('token');
 
@@ -16,6 +16,10 @@ function App() {
 			{/* <Route path='/' element={<Navigate replace to='/login' />} /> */}
 			<Route path='/users/:id/verify/:token' element={<EmailVerify />} />
 			<Route path='/forgot-password' element={<ForgotPassword />} />
+			<Route
+				path='/password-reset/:id/:token'
+				element={<PasswordReset />}
+			/>
 		</Routes>
 	);
 }
