6747d05fb67ff26ad01c6a7e85f7be9cf5ac1053
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 02:14:58 +0530

    checked if token is available for user

--- server/routes/passwordReset.js
+++ server/routes/passwordReset.js
@@ -70,6 +70,12 @@ router.post('/:id/:token', async (req, res) => {
 
 		const user = await User.findOne({ _id: req.params.id });
 		if (!user) return res.status(400).send({ message: 'Invalid link' });
+
+		const token = await Token.findOne({
+			userId: user._id,
+			token: req.params.token,
+		});
+		if (!token) return res.status(400).send({ message: 'Invalid link' });
 	} catch (error) {
 		res.status(500).send({ message: 'Internal Server Error' });
 	}
