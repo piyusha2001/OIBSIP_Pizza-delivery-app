fe62901f38fea93cd28f0125dbd1bec8de067ae5
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 18:49:29 +0530

    navbar imported and rendered

--- client/src/Screens/Main/index.jsx
+++ client/src/Screens/Main/index.jsx
@@ -1,43 +1,44 @@
-import { Flex, HStack, VStack } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
-import styles from './styles.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import Navbar from '../../Components/Navbar/Navbar';
 const Main = () => {
-	let navigate = useNavigate();
-	const user = localStorage.getItem('token');
-	const handleLogout = () => {
-		localStorage.removeItem('token');
-		window.location.reload();
-	};
+	// let navigate = useNavigate();
+	// const user = localStorage.getItem('token');
+	// const handleLogout = () => {
+	// 	localStorage.removeItem('token');
+	// 	window.location.reload();
+	// };
 
 	return (
-		<Flex width='100%'>
-			<VStack width='100%'>
-				<HStack width='100%'>
-					<nav className={styles.navbar}>
-						<h1 style={{ color: 'black' }}>SAY CHEESE!</h1>
-						<div>
-							{user ? (
-								<button
-									className={styles.white_btn}
-									onClick={handleLogout}
-								>
-									Logout
-								</button>
-							) : (
-								<button
-									className={styles.white_btn}
-									onClick={() => {
-										navigate('/login');
-									}}
-								>
-									Login
-								</button>
-							)}
-						</div>
-					</nav>
-				</HStack>
-			</VStack>
-		</Flex>
+		<Navbar />
+		// <Flex width='100%'>
+		// 	<VStack width='100%'>
+		// 		<HStack width='100%'>
+		// 			<nav className={styles.navbar}>
+		// 				<h1 style={{ color: 'black' }}>SAY CHEESE!</h1>
+		// 				<div>
+		// 					{user ? (
+		// 						<button
+		// 							className={styles.white_btn}
+		// 							onClick={handleLogout}
+		// 						>
+		// 							Logout
+		// 						</button>
+		// 					) : (
+		// 						<button
+		// 							className={styles.white_btn}
+		// 							onClick={() => {
+		// 								navigate('/login');
+		// 							}}
+		// 						>
+		// 							Login
+		// 						</button>
+		// 					)}
+		// 				</div>
+		// 			</nav>
+		// 		</HStack>
+		// 	</VStack>
+		// </Flex>
 	);
 };
 
