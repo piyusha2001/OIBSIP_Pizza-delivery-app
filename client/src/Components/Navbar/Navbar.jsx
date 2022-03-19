872a3fa2cb90d552009e28bb974eca811d098ccf
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-19 18:48:47 +0530

    navbar component created

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './styles.css';
+
+export default function navbar() {
+	return (
+		<nav class='navbar navbar-expand-lg navbar-dark custom-navbar'>
+			<div class='container-fluid'>
+				<a class='navbar-brand' href='#'>
+					Navbar
+				</a>
+				<button
+					class='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarNav'
+					aria-controls='navbarNav'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span class='navbar-toggler-icon'></span>
+				</button>
+				<div class='collapse navbar-collapse' id='navbarNav'>
+					<ul class='navbar-nav ms-auto'>
+						<li class='nav-item '>
+							<a
+								class='nav-link active '
+								aria-current='page'
+								href='#'
+							>
+								Login
+							</a>
+						</li>
+						<li class='nav-item '>
+							<a class='nav-link active ' href='#'>
+								Cart
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
