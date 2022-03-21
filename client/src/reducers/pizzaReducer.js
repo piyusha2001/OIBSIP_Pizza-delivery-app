7f4fd620db21861e402b8ba56721f50d655acd21
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-22 03:11:03 +0530

    state set to empty pizza array

--- client/src/reducers/pizzaReducer.js
+++ client/src/reducers/pizzaReducer.js
@@ -1,15 +1,18 @@
-const getAllPizzasReducer = (state = {}, action) => {
+const getAllPizzasReducer = (state = { pizzas: [] }, action) => {
 	switch (action.type) {
 		case 'GET_PIZZAS_REQUEST':
 			return {
+				loading: true,
 				...state,
 			};
 		case 'GET_PIZZAS_SUCCESS':
 			return {
+				loading: false,
 				pizzas: action.payload,
 			};
 		case 'GET_PIZZAS_FAILED':
 			return {
+				loading: false,
 				error: action.payload,
 			};
 		default:
