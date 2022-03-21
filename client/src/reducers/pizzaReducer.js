d08cc16416433ea43a14a3e9c06faa7ec2bce248
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-22 02:09:55 +0530

    reducer created to get all pizzas

---
+++ client/src/reducers/pizzaReducer.js
@@ -0,0 +1,18 @@
+export const getAllPizzasReducer = (state = {}, action) => {
+	switch (action.type) {
+		case 'GET_PIZZAS_REQUEST':
+			return {
+				...state,
+			};
+		case 'GET_PIZZAS_SUCCESS':
+			return {
+				pizzas: action.payload,
+			};
+		case 'GET_PIZZAS_FAILED':
+			return {
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
