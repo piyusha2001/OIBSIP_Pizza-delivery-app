efd961684085da560cf38afb879669534fda0b05
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-22 02:22:37 +0530

    app wraped around provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 ReactDOM.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<ChakraProvider>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</ChakraProvider>
-	</React.StrictMode>,
+	</Provider>,
 	document.getElementById('root'),
 );
