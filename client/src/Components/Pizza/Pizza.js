45069ea86f18b1daedfb3810a08e279fdf206d66
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-21 19:58:13 +0530

    added price logic

--- client/src/Components/Pizza/Pizza.js
+++ client/src/Components/Pizza/Pizza.js
@@ -1,6 +1,8 @@
-import { Box, Image, Select, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Image, Select, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 export default function Pizza({ pizza }) {
+	const [quantity, setQuantity] = useState(1);
+	const [varient, setVarient] = useState('small');
 	return (
 		<Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
 			<Text textAlign='center' fontSize='3xl'>
@@ -16,9 +18,16 @@ export default function Pizza({ pizza }) {
 			<Box p='6'>
 				<Box display='flex' alignItems='baseline'>
 					<Text fontSize='1xl' padding='5px'>
-						Sizes:{' '}
+						Sizes:
 					</Text>
-					<Select size='sm' placeholder='Select'>
+					<Select
+						value={varient}
+						onChange={(e) => {
+							setVarient(e.target.value);
+						}}
+						size='sm'
+						placeholder='Select'
+					>
 						{pizza.varients.map((varient) => {
 							return <option value={varient}>{varient}</option>;
 						})}
@@ -26,11 +35,33 @@ export default function Pizza({ pizza }) {
 					<Text fontSize='1xl' padding='5px'>
 						Quantity:
 					</Text>
-					<Select size='sm' placeholder='Select '>
+					<Select
+						size='sm'
+						value={quantity}
+						onChange={(e) => {
+							setQuantity(e.target.value);
+						}}
+						placeholder='Select '
+					>
 						{[...Array(10).keys()].map((x, i) => {
 							return <option value={i + 1}>{i + 1}</option>;
 						})}
 					</Select>
+				</Box>
+				<Box padding={1}>
+					<Box display='flex' alignItems='baseline'>
+						<Text fontSize='20px' width='50%'>
+							Price: {pizza.prices[0][varient] * quantity}
+						</Text>
+
+						<Button
+							isFullWidth='true'
+							backgroundColor='#b33030'
+							color='white'
+						>
+							ADD TO CART
+						</Button>
+					</Box>
 				</Box>
 			</Box>
 		</Box>
