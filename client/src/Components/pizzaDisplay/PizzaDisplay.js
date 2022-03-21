cafd10ab6fe06c59cb98010c83eee2f0e19c2c40
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-21 19:29:32 +0530

    used simplegrid to display pizzas

--- client/src/Components/pizzaDisplay/PizzaDisplay.js
+++ client/src/Components/pizzaDisplay/PizzaDisplay.js
@@ -1,3 +1,4 @@
+import { SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
 import pizzas from '../../pizzasdata';
 import Pizza from '../Pizza/Pizza';
@@ -5,7 +6,7 @@ import '../PizzaDisplay/styles.css';
 export default function PizzaDisplay() {
 	return (
 		<div>
-			<div className='row'>
+			{/* <div className='row'>
 				{pizzas.map((pizza) => {
 					return (
 						<div className='col-md-4'>
@@ -13,7 +14,17 @@ export default function PizzaDisplay() {
 						</div>
 					);
 				})}
-			</div>
+			</div> */}
+			<SimpleGrid
+				columns={3}
+				marginLeft='80px'
+				marginTop='30px'
+				spacing={4}
+			>
+				{pizzas.map((pizza) => {
+					return <Pizza pizza={pizza} />;
+				})}
+			</SimpleGrid>
 		</div>
 	);
 }
