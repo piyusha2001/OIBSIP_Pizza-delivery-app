2f8799fc2206cab414f0f12f193b1d7b16c407a7
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-22 20:11:28 +0530

    imported cartdisplay component

--- client/src/Screens/CartScreen/CartScreen.js
+++ client/src/Screens/CartScreen/CartScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CartDisplay from '../../Components/CartDisplay/CartDisplay';
 import Navbar from '../../Components/Navbar/Navbar';
 export default function CartScreen() {
 	return (
 		<div>
 			<Navbar />
+			<CartDisplay />
 		</div>
 	);
 }
