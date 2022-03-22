54883002f2e590f6cae0e08c2c9085b3b1ebfdc9
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-22 21:43:02 +0530

    checked if item is aleady added

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,10 +1,22 @@
 export const cartReducer = (state = { cartItems: [] }, action) => {
 	switch (action.type) {
 		case 'ADD_TO_CART':
-			return {
-				...state,
-				cartItems: [...state.cartItems, action.payload],
-			};
+			const alreadyExists = state.cartItems.find(
+				(item) => item._id === action.payload._id,
+			);
+			if (alreadyExists) {
+				return {
+					...state,
+					cartItems: state.cartItems.map((item) =>
+						item._id === action.payload._id ? action.payload : item,
+					),
+				};
+			} else {
+				return {
+					...state,
+					cartItems: [...state.cartItems, action.payload],
+				};
+			}
 		default:
 			return state;
 	}
