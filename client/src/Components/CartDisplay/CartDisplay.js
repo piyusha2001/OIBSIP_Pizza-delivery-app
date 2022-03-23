a1088ad01a0e451b28539dd108098f7a8822a2d7
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-23 17:57:15 +0530

    displayed subtotal

--- client/src/Components/CartDisplay/CartDisplay.js
+++ client/src/Components/CartDisplay/CartDisplay.js
@@ -1,25 +1,35 @@
-import { Box, Flex, HStack, Text, VStack } from '@chakra-ui/react';
+import { Button, Flex, HStack, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CartOrders from '../CartOrders/CartOrders';
 export default function CartDisplay() {
 	const cartstate = useSelector((state) => state.cartReducer);
 	const cartItems = cartstate.cartItems;
+	var subtotal = cartItems.reduce((x, item) => x + item.price, 0);
 	return (
-		<Flex width='100%'>
+		<Flex>
 			<VStack width='100%'>
-				<HStack width='100%'>
-					<VStack width='60%'>
-						<Text padding='10px' fontSize='3xl' fontWeight='bold'>
+				<HStack width='100%' padding='0' marginTop='0px'>
+					<VStack width='70%'>
+						<Text padding='0px' fontSize='3xl' fontWeight='bold'>
 							MY CART
 						</Text>
 						{cartItems.map((item) => {
 							return <CartOrders item={item} />;
 						})}
 					</VStack>
-					<Box width='40%' h='40px' bg='tomato'>
-						2
-					</Box>
+					<VStack>
+						<Text fontSize='3xl' fontWeight='bold'>
+							SUBTOTAL:- {subtotal} /Rs
+						</Text>
+						<Button
+							backgroundColor='#b33030'
+							color='white'
+							width='100%'
+						>
+							PAY
+						</Button>
+					</VStack>
 				</HStack>
 			</VStack>
 		</Flex>
