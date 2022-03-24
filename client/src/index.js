89c53c2f04742db59eb04b19c2982745fa5d84a0
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-24 21:58:32 +0530

    added antd css file

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
 import './index.css';
+import store from './store';
 ReactDOM.render(
 	<Provider store={store}>
 		<ChakraProvider>
