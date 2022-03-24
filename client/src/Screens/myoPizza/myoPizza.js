cfbc21dacac834d7b9bada17ab2357e765723b27
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-24 21:59:35 +0530

    imported myo pizza display component

--- client/src/Screens/myoPizza/myoPizza.js
+++ client/src/Screens/myoPizza/myoPizza.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import MyoPizzaDisplay from '../../Components/MyoPizzaDisplay/MyoPizzaDisplay';
 import Navbar from '../../Components/Navbar/Navbar';
-
 export default function MyoPizza() {
 	return (
 		<div>
 			<Navbar />
-			<myoPizza />
+			<MyoPizzaDisplay />
 		</div>
 	);
 }
