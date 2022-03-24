651f3b0958729affa245ebdbfd5d585b17aaa582
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-24 19:38:33 +0530

    imported myopizza route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const passwordResetRoutes = require('./routes/passwordReset');
 const pizzasRoute = require('./routes/pizzasRoute');
+const myoPizzaRoute = require('./routes/myopizza');
 //connect database
 connection();
 
@@ -19,6 +20,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/password-reset', passwordResetRoutes);
 app.use('/api/pizzas', pizzasRoute);
+app.use('api/myopizza', myoPizzaRoute);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
