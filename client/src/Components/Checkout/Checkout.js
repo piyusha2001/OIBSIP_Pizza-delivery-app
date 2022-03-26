f3a3fa82f5b780bde03814253f093eb0e6f0035d
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-26 23:16:22 +0530

    created button for pay now

---
+++ client/src/Components/Checkout/Checkout.js
@@ -0,0 +1,10 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+
+export default function Checkout({ subtotal }) {
+	return (
+		<Button backgroundColor=' #b33030' color='white' width='100%'>
+			PAY NOW
+		</Button>
+	);
+}
