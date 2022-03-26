44f6169437e8558ee123c52f35ada18f0990872a
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-26 18:13:54 +0530

    condition for normal pizza and myo pizza

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,14 +1,31 @@
 export const addToCart = (pizza, quantity, varient) => (dispatch, getState) => {
-	var cartItem = {
-		name: pizza.name,
-		_id: pizza._id,
-		image: pizza.image,
-		description: pizza.description,
-		varient: varient,
-		quantity: Number(quantity),
-		prices: pizza.prices, //
-		price: pizza.prices[0][varient] * quantity,
-	};
+	// console.log(pizza);
+	console.log('Added2');
+	var cartItem;
+	if (pizza?.name === 'Make your Own Pizza') {
+		cartItem = {
+			name: pizza?.name,
+			_id: pizza?._id,
+			image: pizza?.image,
+			description: pizza?.description,
+			varient: varient,
+			quantity: quantity,
+			prices: pizza?.prices,
+			price: pizza?.prices,
+		};
+	} else {
+		cartItem = {
+			name: pizza?.name,
+			_id: pizza?._id,
+			image: pizza?.image,
+			description: pizza?.description,
+			varient: varient,
+			quantity: Number(quantity),
+			prices: pizza?.prices,
+			price: pizza?.prices[0][varient] * quantity,
+		};
+	}
+	console.log(cartItem);
 	if (cartItem.quantity > 10) {
 		alert('You cannot add more than 10 quantities');
 	} else {
