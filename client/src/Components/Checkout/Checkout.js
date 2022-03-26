bc2d7e001336e329eb759c97bbdf7af6cf858227
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-27 00:41:41 +0530

    imported place order action and dispatched it

--- client/src/Components/Checkout/Checkout.js
+++ client/src/Components/Checkout/Checkout.js
@@ -1,9 +1,17 @@
 import { Button } from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
+import { placeOrder } from '../../actions/orderAction';
 import React from 'react';
 
 export default function Checkout({ subtotal }) {
+	const dispatch = useDispatch();
 	return (
-		<Button backgroundColor=' #b33030' color='white' width='100%'>
+		<Button
+			backgroundColor=' #b33030'
+			color='white'
+			width='100%'
+			onClick={dispatch(placeOrder(subtotal))}
+		>
 			PAY NOW
 		</Button>
 	);
