c44812e6eb629e8626d83db0736453b6964ae83c
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-27 20:19:17 +0530

    create order and verify payment response fetched

--- client/src/actions/orderAction.js
+++ client/src/actions/orderAction.js
@@ -4,13 +4,41 @@ export const placeOrder = (subtotal) => async (dispatch, getState) => {
 	dispatch({ type: 'PLACE_ORDER_ REQUEST ' });
 	const cartItems = getState().cartReducer.cartItems;
 
+	const initPayment = (data) => {
+		const options = {
+			key: 'rzp_test_ZHZ9jDKgl6tz9U',
+			amount: data.amount,
+			currency: data.currency,
+			description: 'Test Transaction',
+			order_id: data.id,
+			handler: async (response) => {
+				try {
+					const { data } = await axios.post(
+						'http://localhost:8080/api/payment/verifypayment',
+						response,
+					);
+					console.log(data);
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			theme: {
+				color: '#3399cc',
+			},
+		};
+		const rzp1 = new window.Razorpay(options);
+		rzp1.open();
+	};
+
 	try {
 		const { data } = await axios.post(
 			'http://localhost:8080/api/payment/orders',
 			{ subtotal, cartItems },
 		);
-		dispatch({ type: 'PLACE_ORDER_SUCCESS' });
+
 		console.log(data);
+		initPayment(data.data);
+		dispatch({ type: 'PLACE_ORDER_SUCCESS' });
 	} catch (error) {
 		dispatch({ type: 'PLACE_ORDER_FAILURE' });
 		console.log(error);
