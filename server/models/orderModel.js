b0f159c1de050369a0b97c2ce29db91768d5e132
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 01:23:31 +0530

    created order model

---
+++ server/models/orderModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const orderSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		userid: { type: String, required: true },
+		orderItems: [],
+		shippingAddress: { type: String, required: true },
+		orderAmount: { type: Number, required: true },
+		isDelivered: { type: Boolean, default: false },
+		transactionId: { type: String, required: true },
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model('orders', orderSchema);
