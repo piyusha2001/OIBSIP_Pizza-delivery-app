645db92e2ed9a9a38a50ec1dcbee7686a696eeef
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 18:20:52 +0530

    created a case to verify order

--- client/src/reducers/orderReducer.js
+++ client/src/reducers/orderReducer.js
@@ -4,12 +4,44 @@ export const placeOrderReducer = (state = {}, action) => {
 			return {
 				loading: true,
 			};
-		case 'PLACE_ORDER_SUCCESS':
+		case 'ORDER_CREATED_SUCCESS':
+			return {
+				loading: true,
+				success: false,
+			};
+		case 'PLACE_ORDER_LOADING':
+			return {
+				loading: true,
+				success: false,
+			};
+		case 'ORDER_VERIFY_SUCCESS':
 			return {
 				loading: false,
 				success: true,
 			};
 		case 'PLACE_ORDER_FAILED':
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return state;
+	}
+};
+
+export const getUserOrdersReducer = (state = { orders: [] }, action) => {
+	switch (action.type) {
+		case 'GET_USER_ORDERS_REQUEST':
+			return {
+				loading: true,
+				...state,
+			};
+		case 'GET_USER_ORDERS_SUCCESS':
+			return {
+				loading: false,
+				orders: action.payload,
+			};
+		case 'GET_USER_ORDERS_FAILED':
 			return {
 				loading: false,
 				error: action.payload,
