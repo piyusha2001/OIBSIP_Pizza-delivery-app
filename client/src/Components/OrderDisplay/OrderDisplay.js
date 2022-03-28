3db3b641897f5d27fab94bddc6994bbce71b4fe4
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 22:56:26 +0530

    created Box for order display

---
+++ client/src/Components/OrderDisplay/OrderDisplay.js
@@ -0,0 +1,105 @@
+import { Box, HStack, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+
+export default function OrderDisplay(order) {
+	return (
+		<Box
+			width='60%'
+			borderWidth='2px'
+			border='solid'
+			padding={3}
+			borderRadius='25px'
+			backgroundColor='#DFDFDE'
+		>
+			<HStack width='100%'>
+				<Text
+					fontSize={'2xl'}
+					width='100%'
+					fontWeight='bold'
+					textAlign='center'
+					color='#b33030'
+				>
+					Items
+				</Text>
+				<Text
+					fontSize={'2xl'}
+					width='100%'
+					fontWeight='bold'
+					textAlign='center'
+					color='#b33030'
+				>
+					Address
+				</Text>
+				<Text
+					fontSize={'2xl'}
+					width='100%'
+					fontWeight='bold'
+					textAlign='center'
+					color='#b33030'
+				>
+					Order Info
+				</Text>
+			</HStack>
+			<hr />
+			<HStack>
+				<VStack width='100%' marginTop={0} textAlign='center'>
+					{order.order.orderItems.map((item) => {
+						return (
+							<Text fontSize='1xl'>
+								{item.name}[{item.varient}] * {item.quantity} =
+								{item.price}
+							</Text>
+						);
+					})}
+				</VStack>
+				<VStack width='100%' textAlign='center'>
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+				</VStack>
+				<VStack width='100%' paddingTop='50px' textAlign='center'>
+					<Text fontSize='1xl'>
+						Order Amount : {order.order.orderAmount}
+					</Text>
+					<Text fontSize='1xl'>
+						Date : {order.order.createdAt.substring(0, 10)}
+					</Text>
+					<Text fontSize='1xl'>Order Id : {order.order.orderId}</Text>
+					<Text fontSize='1xl'>
+						Payment Id : {order.order.paymentId}
+					</Text>
+				</VStack>
+
+				{/* <VStack width='100%'>
+					<Text fontSize={'2xl'} fontWeight='bold'>
+						Items
+					</Text>
+					{order.order.orderItems.map((item) => {
+						return (
+							<Text fontSize='1xl'>
+								{item.name}[{item.varient}] * {item.quantity} =
+								{item.price}
+							</Text>
+						);
+					})}
+				</VStack>
+
+				<VStack width='100%'>
+					<Text fontSize={'2xl'} fontWeight='bold'>
+						Address
+					</Text>
+
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+				</VStack>
+				<VStack width='100%'>
+					<Text fontSize={'2xl'} fontWeight='bold'>
+						Order Info
+					</Text>
+
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+					<Text fontSize='1xl'>{order.order.shippingAddress}</Text>
+				</VStack> */}
+			</HStack>
+		</Box>
+	);
+}
