a92ce60b633d842df5ca38cae12059ba7ac6cfd4
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 22:55:51 +0530

    set alerts acc to orderstate

--- client/src/Components/Checkout/Checkout.js
+++ client/src/Components/Checkout/Checkout.js
@@ -1,20 +1,48 @@
-import { Button } from '@chakra-ui/react';
+import { Alert, AlertIcon, Button, Spinner } from '@chakra-ui/react';
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { placeOrder } from '../../actions/orderAction';
 
 export default function Checkout({ subtotal }) {
+	const orderstate = useSelector((state) => state.placeOrderReducer);
+	const { loading, error, success } = orderstate;
 	const dispatch = useDispatch();
 	return (
-		<Button
-			backgroundColor=' #b33030'
-			color='white'
-			width='100%'
-			onClick={() => {
-				dispatch(placeOrder(subtotal));
-			}}
-		>
-			PAY NOW
-		</Button>
+		<>
+			{loading && (
+				<Spinner
+					thickness='4px'
+					speed='0.65s'
+					emptyColor='gray.200'
+					color='blue.500'
+					size='xl'
+				/>
+			)}
+
+			{error && (
+				<Alert status='error'>
+					<AlertIcon />
+					Something went wrong ! Try again
+				</Alert>
+			)}
+
+			{success && (
+				<Alert status='success'>
+					<AlertIcon />
+					Payment Done ! Order Placed Successfully
+				</Alert>
+			)}
+
+			<Button
+				backgroundColor=' #b33030'
+				color='white'
+				width='100%'
+				onClick={() => {
+					dispatch(placeOrder(subtotal));
+				}}
+			>
+				PAY NOW
+			</Button>
+		</>
 	);
 }
