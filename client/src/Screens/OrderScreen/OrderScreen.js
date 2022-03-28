bed6f55fdb6ad8a4e84d475ea6bc755a2a9fc812
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 17:25:53 +0530

    OrderScreen created

---
+++ client/src/Screens/OrderScreen/OrderScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Orders from '../../Components/Orders/Orders';
+export default function CartScreen() {
+	return (
+		<div>
+			<Navbar />
+			<Orders />
+		</div>
+	);
+}
