c1d0b7aaa97fdfc44acec61236c93a8b5099d0af
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-28 16:34:03 +0530

    order stored to db

--- client/src/actions/orderAction.js
+++ client/src/actions/orderAction.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const placeOrder = (subtotal) => async (dispatch, getState) => {
 	dispatch({ type: 'PLACE_ORDER_ REQUEST ' });
 	const cartItems = getState().cartReducer.cartItems;
+	const user = JSON.parse(localStorage.getItem('user'));
 
 	const initPayment = (data) => {
 		const options = {
@@ -15,12 +16,22 @@ export const placeOrder = (subtotal) => async (dispatch, getState) => {
 				try {
 					const { data } = await axios.post(
 						'http://localhost:8080/api/payment/verifypayment',
+						//post user to backend
 						{
-							response,
+							paymentId: response.razorpay_payment_id,
+							orderId: response.razorpay_order_id,
+							signature: response.razorpay_signature,
+							firstname: user.firstName,
+							lastname: user.lastName,
+							id: user._id,
+							email: user.email,
+							address: user.address,
 							cartItems,
+							subtotal,
 						},
 					);
-					console.log(data);
+
+					console.log(data.message);
 				} catch (error) {
 					console.log(error);
 				}
