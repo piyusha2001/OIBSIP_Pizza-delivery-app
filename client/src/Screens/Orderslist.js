c5d0d04d0b801915869552882195dee9f58b5e7f
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-29 15:43:43 +0530

    created orders list screen

---
+++ client/src/Screens/Orderslist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminScreen from './AdminScreen/AdminScreen';
+
+export default function Orderslist() {
+	return (
+		<>
+			<AdminScreen />
+			<h1>Orderslist</h1>
+		</>
+	);
+}
