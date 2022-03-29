5e351a7c3cf4621d1b3ebda3bbe987e84d10188a
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-29 17:46:30 +0530

    add pizza action created

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -13,3 +13,17 @@ export const getAllPizzas = () => async (dispatch) => {
 		dispatch({ type: 'GET_PIZZAS_FAILED', payload: error });
 	}
 };
+
+export const addPizza = (pizza) => async (dispatch) => {
+	dispatch({ type: 'ADD_PIZZA_REQUEST' });
+	try {
+		const response = await axios.post(
+			'http://localhost:8080/api/pizzas/addpizza',
+			{ pizza },
+		);
+		console.log(response);
+		dispatch({ type: 'ADD_PIZZA_SUCCESS' });
+	} catch (error) {
+		dispatch({ type: 'ADD_PIZZA_FAILED', payload: error });
+	}
+};
