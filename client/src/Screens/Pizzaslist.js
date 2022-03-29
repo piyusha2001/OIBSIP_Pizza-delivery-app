59d7fc61e5b94c9f4111ee5bf27de1b4830fa116
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-29 15:43:57 +0530

    created pizzas list screen

---
+++ client/src/Screens/Pizzaslist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminScreen from './AdminScreen/AdminScreen';
+
+export default function Pizzaslist() {
+	return (
+		<>
+			<AdminScreen />
+			<h1>Pizzas list</h1>
+		</>
+	);
+}
