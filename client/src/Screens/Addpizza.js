10f4eb779404fab2544b9df1b4fa96c69fbc5a6d
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-29 15:43:30 +0530

    created add pizza  screen

---
+++ client/src/Screens/Addpizza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminScreen from './AdminScreen/AdminScreen';
+
+export default function Addpizza() {
+	return (
+		<>
+			<AdminScreen />
+			<h1>Add pizza</h1>
+		</>
+	);
+}
