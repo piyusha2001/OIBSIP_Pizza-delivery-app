482fa17d9b41130e810ebd0bbf4d88164172d736
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-29 15:44:09 +0530

    created users list screen

---
+++ client/src/Screens/Userslist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AdminScreen from './AdminScreen/AdminScreen';
+
+export default function Userslist() {
+	return (
+		<>
+			<AdminScreen />
+			<h1>Users list</h1>
+		</>
+	);
+}
