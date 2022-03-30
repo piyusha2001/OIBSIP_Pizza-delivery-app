54185b153a3a33d899f18bde6313fdbbe9662581
piyusha2001 <ppiyusha2001@gmail.com>
2022-03-30 09:46:03 +0530

    get all toppings action dispatched

--- client/src/Screens/Toppingslist.js
+++ client/src/Screens/Toppingslist.js
@@ -1,14 +1,70 @@
-import { Text } from '@chakra-ui/react';
-import React from 'react';
+import {
+	Spinner,
+	Table,
+	TableCaption,
+	TableContainer,
+	Tbody,
+	Td,
+	Text,
+	Th,
+	Thead,
+	Tr,
+} from '@chakra-ui/react';
+import { Pencil, Trash } from 'phosphor-react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllToppings } from '../actions/myoPizzaAction';
 import AdminScreen from './AdminScreen/AdminScreen';
 
 export default function Toppingslist() {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getAllToppings());
+	}, [dispatch]);
+	const toppingstate = useSelector((state) => state.getAllToppingsReducer);
+
+	const { toppings, loading } = toppingstate;
 	return (
 		<>
 			<AdminScreen />
 			<Text fontSize='2xl' textAlign='center' fontWeight='semibold'>
 				Toppings list
 			</Text>
+			{loading && (
+				<Spinner
+					thickness='4px'
+					speed='0.65s'
+					emptyColor='gray.200'
+					color='blue.500'
+					size='xl'
+				/>
+			)}
+			<TableContainer width='75%' margin='auto'>
+				<Table variant='striped' colorScheme='gray' borderWidth='2px'>
+					<TableCaption>Toppings List</TableCaption>
+					<Thead>
+						<Tr>
+							<Th>Topping name</Th>
+							<Th>Stock</Th>
+
+							<Th>Actions</Th>
+						</Tr>
+					</Thead>
+					<Tbody>
+						{toppings.map((topping) => (
+							<Tr key={topping._id}>
+								<Td>{topping.name}</Td>
+								<Td>{topping.stock}</Td>
+								<Td>
+									<Trash size={22} color='#bc2037' />
+
+									<Pencil size={22} color='#19e672' />
+								</Td>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</TableContainer>
 		</>
 	);
 }
