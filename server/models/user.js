13bfec0ad3bcfaa39fd928c90a62496b00569338
piyusha2001 <ppiyusha2001@gmail.com>
2022-04-09 02:09:56 +0530

    added role to jwt token in generateAuthToken func

--- server/models/user.js
+++ server/models/user.js
@@ -14,9 +14,13 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
-		expiresIn: '7d',
-	});
+	const token = jwt.sign(
+		{ _id: this._id, role: this.role },
+		process.env.JWTPRIVATEKEY,
+		{
+			expiresIn: '7d',
+		},
+	);
 	return token;
 };
 
